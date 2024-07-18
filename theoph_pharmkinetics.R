4635054c562f8011a848b8464279d85dea4a80ea
sallyca530 <sally.ca530@gmail.com>
2024-07-18 14:01:18 -0700

    conc vs time

--- theoph_pharmkinetics.R
+++ theoph_pharmkinetics.R
@@ -7,19 +7,36 @@ data("Theoph")
 head(Theoph)
 tail(Theoph)
 
-subset(Theoph, Subject == 1)
+# The Theoph dataset contains data on the pharmacokinetics of theophylline:
+# - Subject: Identifier of the subject
+# - Time: Time since drug administration (hr)
+# - conc: Plasma theophylline concentration (mg/L)
+# - Weight: Weight of the subject (kg)
+# - Dose: Dose of the drug administered (mg/kg)
 
-sub1 <- subset(Theoph, Subject == 1)
-sub1$conc
+# Plot 1: Plasma concentration vs Time for all subjects
+ggplot(Theoph, aes(x = Time, y = conc, color = factor(Subject))) +
+  geom_line() +
+  labs(title = "Plasma Theophylline Concentration Over Time",
+       x = "Time (hr)",
+       y = "Concentration (mg/L)",
+       color = "Subject") +
+  theme_minimal()
 
-# find mean of concentration of subject 1
-mean(sub1$conc)
 
-# find max of concentration of subject 1
-max(sub1$conc)
-
-# plot subject 1 concentration vs time
-plot(x = sub1$Time, y = sub1$conc)
-
-#plot all subjects concentration vs time
-plot(x = Theoph$Time, y = Theoph$conc, col = Theoph$Subject)
+# subset(Theoph, Subject == 1)
+# 
+# sub1 <- subset(Theoph, Subject == 1)
+# sub1$conc
+# 
+# # find mean of concentration of subject 1
+# mean(sub1$conc)
+# 
+# # find max of concentration of subject 1
+# max(sub1$conc)
+# 
+# # plot subject 1 concentration vs time
+# plot(x = sub1$Time, y = sub1$conc)
+# 
+# #plot all subjects concentration vs time
+# plot(x = Theoph$Time, y = Theoph$conc, col = Theoph$Subject)
